638a6a5aaa212ed45d72ebfc998e4099a67665c8
Hexvel <dimaabdukarimov1234@gmail.com>
2025-01-20 23:11:55 +0600

    first commit

---
+++ components/UserAvatar.tsx
@@ -0,0 +1,27 @@
+import { BadgeCheckIcon } from "lucide-react";
+import Image from "next/image";
+import { avatarUrl } from "./Header";
+
+export default function UserAvatar() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="relative w-[130px] h-[130px]">
+        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-yellow-400 via-red-500 to-purple-600 p-[2px]">
+          <div className="rounded-full bg-black p-1">
+            <Image
+              src={avatarUrl}
+              alt="Profile Pic"
+              width={130}
+              height={130}
+              className="rounded-full"
+            />
+          </div>
+        </div>
+      </div>
+      <span className="text-2xl flex items-center gap-x-2">
+        Hexik Ass <BadgeCheckIcon size={24} className="fill-sky-600" />
+      </span>
+      <span className="text-muted-foreground">@official_hexvel</span>
+    </div>
+  );
+}
---
+++ constants/navbar.constant.ts
@@ -0,0 +1,6 @@
+export const NAVIGATION_ITEMS = [
+  { title: "Search", path: "/search" },
+  { title: "Pages", path: "/pages" },
+  { title: "My Network", path: "/my-network" },
+  { title: "Account", path: "/account" },
+];
